f19c7a8a4a061581ed64b0ba71e6850dc714d090
rdias66 <dias.a@live.com>
2024-06-05 19:02:25 -0300

    initial routing done

--- src/app/Home/page.tsx
+++ src/app/Home/page.tsx
@@ -1,10 +1,13 @@
+import Link from 'next/link'
 import { Button } from '@/components/ui/button'
 
 export default function Home({ LanguageTextData }: any) {
   return (
     <div>
       <h1>this is home </h1>
-      <Button>Redirect to operations</Button>
+      <Link href="/operations">
+        <Button>Go to Operations</Button>
+      </Link>
     </div>
   )
 }
--- src/app/Operations/page.tsx
+++ src/app/Operations/page.tsx
@@ -1,7 +1,13 @@
+import { Button } from '@/components/ui/button'
+import Link from 'next/link'
+
 export default function Operations({ LanguageTextData }: any) {
   return (
     <div>
       <h1>this is operations</h1>
+      <Link href="/">
+        <Button>Go to home</Button>
+      </Link>
     </div>
   )
 }
